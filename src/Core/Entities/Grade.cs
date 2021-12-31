8cfd0ec0fcfceeb62120084ce651f656e192335f
Dheeraj Gupta <dheerajgupta0001.dg@gmail.com>
2021-12-31 12:44:36 +0530

    GradesEntities

---
+++ src/Core/Entities/Grade.cs
@@ -0,0 +1,16 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Grade : AuditableEntity
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ src/Infra/Persistence/Configurations/GradeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Core.Entities;
+
+namespace Infra.Persistence.Configurations
+{
+    public class GradeConfiguration : IEntityTypeConfiguration<Designation>
+    {
+        public void Configure(EntityTypeBuilder<Designation> builder)
+        {
+            // Name is required and just 250 characters
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(250);
+            // Name is unique
+            builder
+            .HasIndex(b => b.Name)
+            .IsUnique();
+        }
+    }
+}
